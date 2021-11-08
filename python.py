7e81a07702bc26f5fef9ff273306a01a90944de8
maximilianriesterer <93677762+maximilianriesterer@users.noreply.github.com>
2021-11-08 20:23:39 +0100

    First commit

---
+++ python.py
@@ -0,0 +1,8 @@
+# Friendster Application
+application_name = "Friendster"
+version = "1.5"
+
+if __name__ == "__main__":
+    print("Welcome to the {0}. Version v{1}".format(application_name, version))
+
+
