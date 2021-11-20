853c72dc00c5776d75597e231ab49ba9c3c1f454
maximilianriesterer <93677762+maximilianriesterer@users.noreply.github.com>
2021-11-20 16:07:08 +0100

    Homework 3

---
+++ Homework 3/Task 2 phone number.py
@@ -0,0 +1,19 @@
+# Python program to check if given mobile number is valid
+import re
+
+def isValid(s):
+	
+	# 1) Begins with 0 or 46
+	# 2) Then contains 7 
+	# 3) Then contains 8 digits
+	Pattern = re.compile("(0|46)?[7][0-9]{8}")
+	return Pattern.match(s)
+
+# Phone number
+s = "0730507051"
+if (isValid(s)):
+	print ("Valid Number")	
+else :
+	print ("Invalid Number")
+
+
---
+++ Homework 3/Task 3 name check.py
@@ -0,0 +1,13 @@
+# Python program to compare strings ignore case
+
+# Goal name
+string1 = 'Maximilian'
+
+# write the name here
+string2 = 'MAXIMILIAN'
+  
+# check strings are same using upper() function
+if(string1.upper() == string2.upper()):
+    print('Thank you, Maximilian is the correct name')
+else:
+    print('Wrong name, write Maximilian')
